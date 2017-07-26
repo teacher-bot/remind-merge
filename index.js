31f0f06e433c35f7c52792e752800d0205e5cbdb
Jason Etcovitch <jasonetco@github.com>
2017-07-26 15:12:01 -0700

    Use  module

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const configurable = require('configurable');
+
 /**
  * @typedef {Object} Config
  * @prop {string} message
@@ -5,9 +7,11 @@
  * Comments to remind a user to delete the branch from a merged pull request
  * @param {Object} robot
  * @param {Config} config
+ * @param {string} [configFilename]
  */
-module.exports = (robot, config) => {
+module.exports = (robot, defaults = {}, configFilename = 'remind-merge.yml') => {
   robot.on('pull_request.closed', async context => {
+    const config = await configurable(context, defaults, configFilename);
     return context.github.issues.createComment(context.repo({
       number: context.payload.number,
       body: config.message
