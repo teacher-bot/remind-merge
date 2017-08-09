9cba1bd77d2d6e25b86aef95663386cb983c4d30
Briana Swift <brianamarie@github.com>
2017-08-09 10:28:10 -0500

    now it changes the message based on if it was merged or closed

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const defaultsShape = {
   message: 'string',
 };
 
+/*/
 function checkForDefaults(defaults) {
   const errors = Object.keys(defaultsShape).filter(key => !Object.prototype.hasOwnProperty.call(defaults, key));
   if (errors.length > 0) errors.forEach(err => console.error(`Key \`${err}\` of type \`${defaultsShape[err]}\` is missing.`));
-}
+}*/
 
-/**
+
+/*
  * @typedef {Object} Config
  * @prop {string} message
  *
@@ -17,24 +19,48 @@ function checkForDefaults(defaults) {
  * @param {String} [configFilename]
  */
 module.exports = (robot, defaults, configFilename = 'remind-merge.yml') => {
-  checkForDefaults(defaults);
+  //checkForDefaults(defaults);
 
   robot.on('pull_request.closed', async context => {
-    const {number} = context.payload;
+    const {
+      number
+    } = context.payload;
+
+    let merged = context.payload.pull_request.state
 
     let config;
     try {
-      const {remindMerge} = await context.config(configFilename);
+      const {
+        remindMerge
+      } = await context.config(configFilename);
       config = Object.assign({}, defaults, remindMerge);
     } catch (err) {
       config = defaults;
     }
 
-    return context.github.issues.createComment(context.repo({
-      number,
-      body: config.message
-    }));
+
+    function commentAndCleanup (){
+      if (merged == "merged") {
+        return context.github.issues.createComment(context.repo({
+          number,
+          body: "Congratulations! You merged your branch! Now, please delete your branch. If you don't delete your branch within two minutes, I'll go ahead and delete it for you."
+        }));
+      } else if (merged == "closed"){
+        return context.github.issues.createComment(context.repo({
+          number,
+          body: "Are you sure you want to close this pull request without merging it? If you don't re-open this pull request or delete your branch within two minutes, I'll go ahead and delete your branch for you. Good luck!"
+        }));
+      }
+    }
+
+    commentAndCleanup();
+
+    // call the function with a timeout
+    //setTimeout(alert("4 seconds"),4000);
+
+
   });
+
 
   console.log('Yay, the teacher-bot/remind-merge plugin was loaded!');
 
